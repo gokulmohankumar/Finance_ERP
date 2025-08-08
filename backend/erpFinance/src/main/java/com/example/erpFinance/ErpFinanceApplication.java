cdcd533d6d4f3b4e7dc1c7c5b85b8cdb0f3df0d0
gokulmohankumar <mohangokul4469@gmail.com>
2025-08-08 14:08:22 +0530

    Project initialized

---
+++ backend/erpFinance/src/main/java/com/example/erpFinance/ErpFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.erpFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpFinanceApplication.class, args);
+	}
+
+}
---
+++ backend/erpFinance/src/test/java/com/example/erpFinance/ErpFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erpFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
